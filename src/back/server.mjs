8c899106774689da99b3d9b4e925134272a1dd95
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-07-31 18:59:33 +0530

    ...Maybe having endpoints parsed is okay.
    ...Since they're less in number! ...And because I want a fast way to tell apart an endpoint and a file.

--- src/back/server.mjs
+++ src/back/server.mjs
@@ -2,29 +2,12 @@ import mime from "mime";
 import fs from "node:fs";
 import url from "node:url";
 import http from "node:http";
-import { error } from "node:console";
 
 const s_port = 8080;
-
-// Separating properties is significantly faster:
-// const s_serverEndpoints = await loadServerConfigJsonFile("endpoints");
-// const s_serverResources = await loadServerConfigJsonFile("resources");
-// console.log("[SERVER] Loaded all configuration JSON files.");
-
 let s_requestCount = 0;
 
-// async function loadServerConfigJsonFile(p_path) {
-// 	try {
-// 		const file = await fs.promises.readFile(`./config/${p_path}.json`);
-// 		return JSON.parse(file);
-// 	} catch (e) {
-// 		console.error(`[SERVER] Failed to load JSON configuration file at \`${p_path}\`.`, e);
-// 		process.exit(1);
-// 	}
-// }
-
 function failRequest(p_response, p_logTag, p_statusCode, p_message) {
-	const message = `\`${p_statusCode}\`! ${p_message}.`;
+	const message = `\`${p_statusCode}\`! ${p_message}`;
 	p_response.writeHead(404, "Content-Type", "text/plain");
 	console.error(p_logTag + message);
 	p_response.end(message);
@@ -35,12 +18,7 @@ const s_server = http.createServer(async (p_request, p_response) => {
 	const requestNumber = ++s_requestCount;
 	const httpPath = httpArgs.pathname;
 
-	const requestLogTag = `[REQUEST #${requestNumber}]`;
-	// const mapping = s_serverResources[httpPath];
-
-	// So, if this file exists:
-	// if (mapping != undefined) {
-	// const filePath = `./src/front/${mapping}`;
+	const requestLogTag = `[REQUEST #${requestNumber}] `;
 
 	fs.readFile(`./src/front/${httpPath}`, (p_error, p_data) => {
 		if (p_error) {
@@ -48,53 +26,40 @@ const s_server = http.createServer(async (p_request, p_response) => {
 			return;
 		}
 
-		console.log(requestLogTag + "`200`. Everything went well.");
+		console.log(requestLogTag + "`200`. Everything went well!...");
 		p_response.writeHead(200, "Content-Type", mime.getType(filePath));
 		p_response.end(p_data);
 	});
 
-	// return;
-	// }
-
-	// It doesn't? See if the URL lands at an API endpoint instead:
-	// const endpoint = s_serverEndpoints[httpPath];
-
-	// Not an API endpoint either? Welp!:
-	if (httpPath == undefined) {
-		failRequest(p_response, requestLogTag, 404, "Endpoint does not exist.");
-		return;
-	}
-
-	// Otherwise, yeah, it's an API endpoint, do it's thing:
 	try {
 		const module = await import(`./src/back/endpoints/${httpPath}.mjs`);
 		const httpMethod = httpArgs.method;
 
 		// Get the function implementing given HTTP method from said module!:
 		try {
-			const method = module[httpMethod];
+			const methodImpl = module[httpMethod];
 
 			// Call the function!:
 			try {
-				method(httpArgs, p_request, p_response);
+				methodImpl(requestNumber, httpArgs, p_request, p_response);
 			} // HTTP method implementation *threw up?:*
 			catch (p_error) {
-				failRequest(p_response, requestLogTag, 500, "Internal server error.");
+				failRequest(p_response, requestLogTag, 500, "Internal Server Error.");
 				console.error(requestLogTag + p_error);
 			}
 
 		} // HTTP method doesn't exist in module?:
 		catch (p_error) {
-			failRequest(p_response, requestLogTag, 405, `Method \`${httpMethod}\` was not implemented.`);
+			failRequest(p_response, requestLogTag, 405, `Method \`${httpMethod}\` Not Allowed.`);
 			throw new Error(`Method not found in module \`${module}\`.`);
 		}
 	} // Module doesn't exist?!:
 	catch (p_error) {
 		console.error(`Module \`${module}\` not found.`, e);
-		failRequest(p_response, requestLogTag, 501, "Endpoint not implemented.");
+		failRequest(p_response, requestLogTag, 501, "Endpoint Not Implemented.");
 	}
 });
 
 s_server.listen(s_port, () => {
-	console.log(`[SERVER] Server has started on port \`${s_port}\`.`);
+	console.log(`[SERVER] Server has started on URL \`https://localhost:${s_port}\`.`)
 });
