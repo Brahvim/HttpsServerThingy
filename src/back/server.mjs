beb473aeeef4df8ca3a5ee92219766477b92bad2
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-04 12:10:49 +0530

    Watching directories isn't as good of an idea, I guess.

--- src/back/server.mjs
+++ src/back/server.mjs
@@ -4,19 +4,29 @@ import url from "node:url";
 import http from "node:http";
 
 const s_port = 8080;
-const s_endpoints = loadServerConfigJsonFile();
+const s_endpointImplsIndex = fs.readdirSync("./src/back/endpoints")
+	.filter(p_fileName => p_fileName.endsWith(".mjs"));
+const s_endpointsDirWatch = fs.watch("./src/back/endpoints", (p_event, p_fileName) => {
+	if (!p_fileName.endsWith(".mjs") || p_event != "rename")
+		return;
+
+	const id = s_endpointImplsIndex.findIndex(p_toFind => p_toFind == p_fileName);
+
+	if (id == -1) {
+		s_endpointImplsIndex.push(p_fileName);
+		console.log(`[WATCHER] File \`${p_fileName}\` was added.`);
+	} else {
+		s_endpointImplsIndex.splice(id, 1);
+		console.log(`[WATCHER] File \`${p_fileName}\` was removed.`);
+	}
+});
+
 let s_requestCount = 0;
 
-function loadServerConfigJsonFile(p_fileNameNoExt) {
-	try {
-		const path = `./config/${p_fileNameNoExt}.json`;
-		const file = fs.readFileSync(path);
-		return JSON.parse(file);
-	} catch (e) {
-		console.error(`[SERVER] Failed to load JSON configuration file at \`${p_path}\`.`, e);
-		process.exit(1);
-	}
-}
+process.on("exit", () => {
+	fs.unwatchFile(s_endpointsDirWatch);
+	console.log(`[SERVER] Server has stopped.`);
+});
 
 const s_server = http.createServer(async (p_request, p_response) => {
 	const httpArgs = url.parse(p_request.url, true);
@@ -41,24 +51,12 @@ ${message}
 		);
 	}
 
-	fs.readFile(`./src/front/${httpPath}`, (p_error, p_data) => {
-		if (p_error) {
-			failRequest(404, "File not found.");
-			return;
-		}
-
-		console.log(requestLogTag + "`200`. Everything went well!...");
-		p_response.writeHead(200, "Content-Type", mime.getType(filePath));
-		p_response.end(p_data);
-	});
-
 	try {
 		const module = await import(`./src/back/endpoints/${httpPath}.mjs`);
-		const httpMethod = httpArgs.method;
 
 		// Get the function implementing given HTTP method from said module!:
 		try {
-			const methodImpl = module[httpMethod];
+			const methodImpl = module[httpArgs.method];
 
 			// Call the function!:
 			try {
@@ -66,21 +64,33 @@ ${message}
 			} // HTTP method implementation *threw up?:*
 			catch (p_error) {
 				failRequest(500, "Internal Server Error.");
-				console.error(requestLogTag + p_error);
 			}
 
 		} // HTTP method doesn't exist in module?:
 		catch (p_error) {
 			failRequest(405, `Method \`${httpMethod}\` Not Allowed.`);
-			throw new Error(`Method not found in module \`${module}\`.`);
+			// Let's not crash, okay!?
+			// throw new Error(`Method not found in module \`${module}\`.`);
 		}
 	} // Module doesn't exist?!:
 	catch (p_error) {
-		console.error(`Module \`${module}\` not found.`, e);
-		failRequest(501, "Endpoint Not Implemented.");
+		// console.error(`Module \`${module}\` not found.`, e);
+		// failRequest(501, "Endpoint Not Implemented.");
+
+		fs.readFile(`./src/front/${httpPath}`, (p_error, p_data) => {
+			if (p_error) {
+				failRequest(404, "File not found.");
+				return;
+			}
+
+			console.log(requestLogTag + "`200`. Everything went well!...");
+			p_response.writeHead(200, "Content-Type", mime.getType(filePath));
+			p_response.end(p_data);
+		});
 	}
 });
 
 s_server.listen(s_port, () => {
-	console.log(`[SERVER] Server has started on URL \`https://localhost:${s_port}\`.`)
+	console.log(`[SERVER] Loaded modules: \`{}\`.`);
+	console.log(`[SERVER] Server has started on URL \`https://localhost:${s_port}\`.`);
 });
